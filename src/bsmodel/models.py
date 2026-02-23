02d51c28e842c5aabf400c228c19bbbdee8944fc
Vojta Bartos <bartos.v@icloud.com>
2026-02-24 00:24:01 +0100

    Implemented sensitivities and refined tests

--- src/bsmodel/models.py
+++ src/bsmodel/models.py
@@ -12,32 +12,52 @@ class BSModel:
         rate: float,
         dividend: float = 0,
     ):
-        self.spt = spot
-        self.stk = strike
-        self.tte = expiration / 365
-        self.vol = volatility / 100
-        self.rfr = rate / 100
-        self.dvd = dividend / 100
+        self.spt: float = spot
+        self.stk: float = strike
+        self.tte: float = expiration / 365
+        self.vol: float = volatility
+        self.rfr: float = rate
+        self.dvd: float = dividend
 
         # Pre-calculate d1 and d2 for reuse
         self._calculate_d1_d2()
 
-    def _calculate_d1_d2(self):
+    def _calculate_d1_d2(self) -> None:
         if self.tte <= 0:
             self.d1, self.d2 = 0, 0
             return
 
-        self.d1 = (log(self.spt / self.stk) + (self.rfr - self.dvd + 0.5 * self.vol**2) * self.tte) / (
+        self.d1: float = (log(self.spt / self.stk) + (self.rfr - self.dvd + 0.5 * self.vol**2) * self.tte) / (
             self.vol * sqrt(self.tte)
         )
-        self.d2 = self.d1 - self.vol * sqrt(self.tte)
+        self.d2: float = self.d1 - self.vol * sqrt(self.tte)
 
-    def price_call(self):
-        return self.spt * exp(-self.dvd * self.tte) * norm.cdf(self.d1) - self.stk * exp(
-            -self.rfr * self.tte
-        ) * norm.cdf(self.d2)
+    def price(self, option_type:str = "call") -> float:
+        if option_type == "call":
+            return self.spt * exp(-self.dvd * self.tte) * norm.cdf(self.d1) - self.stk * exp(
+                -self.rfr * self.tte
+                ) * norm.cdf(self.d2)
+        else: # Put
+            return self.stk * exp(-self.rfr * self.tte) * norm.cdf(-self.d2) - self.spt * exp(
+                -self.dvd * self.tte
+            ) * norm.cdf(-self.d1)
 
-    def price_put(self):
-        return self.stk * exp(-self.rfr * self.tte) * norm.cdf(-self.d2) - self.spt * exp(
-            -self.dvd * self.tte
-        ) * norm.cdf(-self.d1)
+    def delta(self, option_type:str = "call") -> float:
+        return exp(-self.dvd * self.tte) * norm.cdf(self.d1)
+
+    def gamma(self, option_type:str = "call") -> float:
+        return exp(-self.dvd * self.tte) * norm.pdf(self.d1) / (self.spt * self.vol * sqrt(self.tte))
+
+    def vega(self, option_type:str = "call") -> float:
+        return self.spt * exp(-self.dvd * self.tte) * norm.pdf(self.d1) * sqrt(self.tte) / 100
+
+    def theta(self, option_type:str = "call") -> float:
+        if self.tte <= 0:
+            return 0.0
+
+        return (-(self.spt * exp(-self.dvd*self.tte) * norm.pdf(self.d1) * self.vol) / (2 * sqrt(self.tte))
+         - self.rfr * self.stk * exp(-self.rfr*self.tte) * norm.cdf(self.d2)
+         + self.dvd * self.spt * exp(-self.dvd*self.tte) * norm.cdf(self.d1)) / 365
+
+    def rho(self, option_type:str = "call") -> float:
+        return self.stk * self.tte * exp(-self.rfr * self.tte) * norm.cdf(self.d2) / 100
--- tests/test_bsmodel.py
+++ tests/test_bsmodel.py
@@ -2,120 +2,105 @@ from bsmodel.models import BSModel
 from math import log, sqrt
 import pytest
 
-
-class TestBSModel:
-    @pytest.mark.unit
-    @pytest.mark.parametrize(
-        "inputs, expected_result",
+@pytest.mark.parametrize(
+        "inputs, px, delta, gamma, vega, theta, rho",
         [
-            ([100, 95, 365, 30, 10, 3], 17.32),
-            ([100, 105, 365, 30, 10, 3], 12.50),
-            ([100, 95, 5, 30, 10, 3], 5.19),
-            ([100, 95, 365, 15, 10, 3], 12.70),
-            ([100, 95, 365, 30, 5, 3], 14.82),
-            ([100, 95, 365, 30, 10, 1], 18.74),
+            ([100.0,  100.0,   365,  0.200,  0.05,  0.00], 10.4506,  0.6368,  0.0188,  0.3752,  -0.0176,  0.5323),
+            ([150.0,  100.0,   365,  0.200,  0.05,  0.00], 54.9701,  0.9913,  0.0008,  0.0355,  -0.0138,  0.9372),
+            ([ 50.0,  100.0,   365,  0.200,  0.05,  0.00],  0.0024,  0.0009,  0.0003,  0.0016,  -0.0000,  0.0004),
+            ([100.0,  100.0,     1,  0.200,  0.05,  0.00],  0.4245,  0.5073,  0.3810,  0.0209,  -0.2157,  0.0014),
+            ([100.0,  100.0,  1825,  0.200,  0.05,  0.00], 29.1386,  0.7831,  0.0066,  0.6567,  -0.0103,  2.4584),
+            ([100.0,  100.0,   365,  0.800,  0.05,  0.00], 32.8210,  0.6781,  0.0045,  0.3585,  -0.0441,  0.3499),
+            ([100.0,  100.0,   365,  0.200,  0.00,  0.00],  7.9656,  0.5398,  0.0198,  0.3970,  -0.0109,  0.4602),
+            ([100.0,  100.0,   365,  0.200,  0.50,  0.00], 39.3780,  0.9953,  0.0007,  0.0136,  -0.0828,  0.6016),
+            ([100.0,  100.0,   365,  0.001,  0.05,  0.00],  4.8771,  1.0000,  0.0000,  0.0000,  -0.0130,  0.9512),
+            ([100.0,  100.0,   365,  0.200,  0.05,  0.03],  8.6525,  0.5621,  0.0190,  0.3795,  -0.0123,  0.4756),
+            ([100.0,  100.0,   365,  0.200,  0.05,  0.08],  6.1430,  0.4432,  0.0184,  0.3678,  -0.0056,  0.3817),
         ],
-    )
-    def test_price_call(self, inputs, expected_result):
-        model = BSModel(*inputs)
-        call_price = model.price_call()
+)
+class TestBSModelCall:
+    @pytest.fixture(autouse=True)
+    def setup(self, inputs, px, delta, gamma, vega, theta, rho):
+        self.model = BSModel(*inputs)
 
-        assert call_price == pytest.approx(expected_result, abs=0.005)
-
-    @pytest.mark.unit
-    @pytest.mark.parametrize(
-        "inputs, expected_result",
-        [
-            ([100, 95, 365, 30, 10], 19.47),
-            ([100, 105, 365, 30, 10], 14.29),
-            ([100, 95, 5, 30, 10], 5.23),
-            ([100, 95, 365, 15, 10], 15.18),
-            ([100, 95, 365, 30, 5], 16.80),
-        ],
-    )
-    def test_price_call_zero_dividend(self, inputs, expected_result):
-        model = BSModel(*inputs)
-        call_price = model.price_call()
-
-        assert call_price == pytest.approx(expected_result, abs=0.005)
+        self.px = px
+        self.delta = delta
+        self.gamma = gamma
+        self.vega = vega
+        self.theta = theta
+        self.rho = rho
 
     @pytest.mark.unit
-    @pytest.mark.parametrize(
-        "inputs, expected_result",
-        [
-            ([100, 95, 365, 30, 10, 3], 6.24),
-            ([100, 105, 365, 30, 10, 3], 10.46),
-            ([100, 95, 30, 30, 10, 3], 1.27),
-            ([100, 95, 365, 15, 10, 3], 1.62),
-            ([100, 95, 365, 30, 5, 3], 8.15),
-            ([100, 95, 365, 30, 10, 1], 5.69),
-        ],
-    )
-    def test_price_put(self, inputs, expected_result):
-        model = BSModel(*inputs)
-        put_price = model.price_put()
-
-        assert put_price == pytest.approx(expected_result, abs=0.005)
+    def test_price_call(self):
+        assert self.model.price(option_type="call") == pytest.approx(self.px, abs=0.005)
 
     @pytest.mark.unit
-    @pytest.mark.parametrize(
-        "inputs, expected_result",
-        [
-            ([100, 95, 365, 30, 10], 5.43),
-            ([100, 105, 365, 30, 10], 9.30),
-            ([100, 95, 5, 30, 10], 0.10),
-            ([100, 95, 365, 15, 10], 1.14),
-            ([100, 95, 365, 30, 5], 7.17),
-        ],
-    )
-    def test_price_put_zero_dividend(self, inputs, expected_result):
-        model = BSModel(*inputs)
-        put_price = model.price_put()
-
-        assert put_price == pytest.approx(expected_result, abs=0.005)
+    def test_delta_call(self):
+        assert self.model.delta(option_type="call") == pytest.approx(self.delta, abs=0.005)
 
     @pytest.mark.unit
-    @pytest.mark.parametrize(
-        "inputs",
-        [
-            ([100, 95, 0, 30, 10]),
-            ([100, 105, -1, 30, 10]),
-        ],
-    )
-    def test_d1_d2_edge_cases(self, inputs):
-        model = BSModel(*inputs)
-
-        model._calculate_d1_d2()
-
-        assert model.d1 == 0
-        assert model.d2 == 0
+    def test_gamma_call(self):
+        assert self.model.gamma(option_type="call") == pytest.approx(self.gamma, abs=0.005)
 
     @pytest.mark.unit
-    @pytest.mark.parametrize(
-        "inputs",
-        [
-            ([100, 95, 365, 30, 10, 3]),
-            ([100, 105, 365, 30, 10, 3]),
-            ([100, 95, 5, 30, 10, 1]),
-            ([100, 95, 365, 15, 10, 0]),
-            ([100, 95, 365, 30, 5, 0]),
-        ],
+    def test_vega_call(self):
+        assert self.model.vega(option_type="call") == pytest.approx(self.vega, abs=0.005)
+
+    @pytest.mark.unit
+    def test_theta_call(self):
+        assert self.model.theta(option_type="call") == pytest.approx(self.theta, abs=0.005)
+
+    @pytest.mark.unit
+    def test_rho_call(self):
+        assert self.model.rho(option_type="call") == pytest.approx(self.rho, abs=0.005)
+
+    @pytest.mark.unit
+    def test_call_put_parity(self):
+        pass
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "inputs",
+    [
+        ([100, 95, 0, 30, 10]),
+        ([100, 105, -1, 30, 10]),
+    ],
+)
+def test_d1_d2_edge_cases(inputs):
+    model = BSModel(*inputs)
+
+    model._calculate_d1_d2()
+
+    assert model.d1 == 0
+    assert model.d2 == 0
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "inputs",
+    [
+        ([100, 95, 365, 30, 10, 3]),
+        ([100, 105, 365, 30, 10, 3]),
+        ([100, 95, 5, 30, 10, 1]),
+        ([100, 95, 365, 15, 10, 0]),
+        ([100, 95, 365, 30, 5, 0]),
+    ],
+)
+def test_d1_d2(inputs):
+    model = BSModel(*inputs)
+
+    spot = inputs[0]
+    strike = inputs[1]
+    expiration = inputs[2] / 365
+    volatility = inputs[3]
+    rate = inputs[4]
+    dividend = inputs[5]
+    model._calculate_d1_d2()
+
+    expected_d1 = (log(spot / strike) + (rate - dividend + 0.5 * volatility**2) * expiration) / (
+        volatility * sqrt(expiration)
     )
-    def test_d1_d2(self, inputs):
-        model = BSModel(*inputs)
+    expected_d2 = expected_d1 - volatility * sqrt(expiration)
 
-        spot = inputs[0]
-        strike = inputs[1]
-        expiration = inputs[2] / 365
-        volatility = inputs[3] / 100
-        rate = inputs[4] / 100
-        dividend = inputs[5] / 100
-        model._calculate_d1_d2()
+    assert model.d1 == pytest.approx(expected_d1)
+    assert model.d2 == pytest.approx(expected_d2)
 
-        expected_d1 = (log(spot / strike) + (rate - dividend + 0.5 * volatility**2) * expiration) / (
-            volatility * sqrt(expiration)
-        )
-        expected_d2 = expected_d1 - volatility * sqrt(expiration)
-
-        assert model.d1 == pytest.approx(expected_d1)
-        assert model.d2 == pytest.approx(expected_d2)
